d6e1897f9e79bf1bf0983bd9a360a3def07df6b8
Cacho Medina <zanlorenzocacho10@gmail.com>
2024-04-24 00:22:59 -0300

    arreglos en page 2

--- src/app/pagina1/page.jsx
+++ src/app/pagina1/page.jsx
@@ -2,12 +2,13 @@
 import { motion } from "framer-motion";
 
 function page() {
+    const width = window.innerWidth;
     return (
         <motion.div
             className="bg-blue-700 h-screen grid place-items-center"
             initial={{ width: 0 }}
             animate={{ width: "100%" }}
-            exit={{ x: window.innerWidth, transition: { duration: 0.3 } }}
+            exit={{ x: width, transition: { duration: 0.3 } }}
         >
             <h1 className="text-white text-center font-bold text-7xl">
                 Entrada con Framer Motion
