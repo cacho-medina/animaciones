c9d23e0b24f7327441faad7dbe414ccb885035dd
Cacho Medina <zanlorenzocacho10@gmail.com>
2024-04-24 00:31:30 -0300

    se cambio animacion en page 1

--- src/app/pagina1/page.jsx
+++ src/app/pagina1/page.jsx
@@ -2,13 +2,13 @@
 import { motion } from "framer-motion";
 
 function page() {
-    const width = window.innerWidth;
     return (
         <motion.div
             className="bg-blue-700 h-screen grid place-items-center"
-            initial={{ width: 0 }}
-            animate={{ width: "100%" }}
-            exit={{ x: width, transition: { duration: 0.3 } }}
+            /* initial={{ width: 0 }}
+            animate={{ width: "100%" }} */
+            initial={{ x: "-100%" }}
+            animate={{ x: "0" }}
         >
             <h1 className="text-white text-center font-bold text-7xl">
                 Entrada con Framer Motion
